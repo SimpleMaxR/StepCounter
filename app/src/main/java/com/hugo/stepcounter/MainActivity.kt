04ba77f44de07eb7f664de1da9c1ea1dbf76bbd7
Hugo Ho <hugoho912@outlook.com>
2024-05-23 11:42:19 +0800

    Better code comments & Optimize import

--- app/src/main/java/com/hugo/stepcounter/MainActivity.kt
+++ app/src/main/java/com/hugo/stepcounter/MainActivity.kt
@@ -11,31 +11,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hugo.stepcounter.ui.theme.StepCounterTheme
-import kotlin.math.log
 
 class MainActivity : ComponentActivity(), SensorEventListener {
-    private lateinit var mSensorManager: SensorManager
-    private lateinit var mStepCounter: Sensor
-    private lateinit var mStepDetector: Sensor
-    private var step by mutableStateOf(0)
-    private var stepCount by mutableStateOf(0)
+    private lateinit var mSensorManager: SensorManager  // 传感器管理器
+    private lateinit var mStepCounter: Sensor   // 计步传感器
+    private lateinit var mStepDetector: Sensor  // 检测传感器
+    private var step by mutableStateOf(0)   // 步数
+    private var stepCount by mutableStateOf(0)  // 总共步数
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,23 +46,25 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     override fun onResume() {
         super.onResume()
         Log.i("onResume", "onResume: ")
+
+        // 获取传感器
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             mStepCounter = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!!
         } else {
             Log.e("onResume", "onResume: 没有找到传感器 stepCounter")
         }
-
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
             mStepDetector = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!!
         } else {
             Log.e("onResume", "onResume: 没有找到传感器 stepDetector")
         }
+
+        // 注册监听器
         mSensorManager.registerListener(
             this,
             mStepCounter,
             SensorManager.SENSOR_DELAY_NORMAL
         )
-
         mSensorManager.registerListener(
             this,
             mStepDetector,
@@ -79,6 +74,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
     override fun onPause() {
         super.onPause()
+        // 取消监听
         mSensorManager.unregisterListener(this)
     }
 
@@ -86,8 +82,10 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         Log.i("onAccuracyChanged", "onAccuracyChanged: $accuracy")
     }
 
+    // onSensorChanged 传感器数据变化
     override fun onSensorChanged(event: SensorEvent?) {
         Log.i("onSensorChanged", "onSensorChanged: ")
+        // 针对不同的传感器类型做不同的处理
         when (event?.sensor?.type) {
             Sensor.TYPE_STEP_DETECTOR -> {
                 if (event.values[0] == 1.0f) {
@@ -101,13 +99,13 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     }
 }
 
-class CircularTransitionData(
-    progress: State<Float>,
-    color: State<Color>
-) {
-    val progress by progress
-    val color by color
-}
+//class CircularTransitionData(
+//    progress: State<Float>,
+//    color: State<Color>
+//) {
+//    val progress by progress
+//    val color by color
+//}
 
 //@Composable
 //fun CircularProgress(
